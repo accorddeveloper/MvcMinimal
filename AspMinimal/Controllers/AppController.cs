7ef9493f07c86b543d92b5261ba37880e12c2e13
Luke Muccillo <luke@vibrato.com.au>
2016-11-30 13:52:45 +1100

    Removed obj and bin files and added header serialization to the main app controller

--- AspMinimal/Controllers/AppController.cs
+++ AspMinimal/Controllers/AppController.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using System.Configuration;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Web.Http;
@@ -23,44 +24,54 @@ namespace AspMinimal.Controllers
 
     public class AppController : ApiController
     {
+        public static string VersionKey = "CurrentVersion";
+
+        public HttpResponseMessage CreateResponse(string path)
+        {
+            var theHeaders = Request.Headers;
+            var thePayload = new
+            {
+                message = string.Format("Hello from {0} v{1}", 
+                path, ConfigurationManager.AppSettings.Get(VersionKey)),
+                headers = theHeaders
+            };
+
+            return ResponseMessageFactory.Create(thePayload, System.Net.HttpStatusCode.OK);
+        }
+
         [Route("")]
         [HttpGet]
         public object RootPath()
         {
-            var payload = new { message = "Hello from / (root path)" };
-            return ResponseMessageFactory.Create(payload, System.Net.HttpStatusCode.OK);
+            return CreateResponse("/ (root path)");
         }
 
         [Route("session/")]
         [HttpGet]
-        public object BetSlips()
+        public object Session()
         {
-            var payload = new { message = "Hello from /session" };
-            return ResponseMessageFactory.Create(payload, System.Net.HttpStatusCode.OK);
+            return CreateResponse("/session");
         }
 
         [Route("bet_slips/finalise")]
         [HttpGet]
         public object BetSlipsFinalise()
         {
-            var payload = new { message = "Hello from /bet_slips/finalise" };
-            return ResponseMessageFactory.Create(payload, System.Net.HttpStatusCode.OK);
+            return CreateResponse("/bet_slips/finalise");
         }
 
         [Route("betting/")]
         [HttpGet]
         public object Betting()
         {
-            var payload = new { message = "Hello from /betting" };
-            return ResponseMessageFactory.Create(payload, System.Net.HttpStatusCode.OK);
+            return CreateResponse("/betting");
         }
 
         [Route("betting/notice")]
         [HttpGet]
         public object BettingNotice()
         {
-            var payload = new { message = "Hello from /betting/notice" };
-            return ResponseMessageFactory.Create(payload, System.Net.HttpStatusCode.OK);
+            return CreateResponse("/betting/notice");
         }
     }
 }
--- AspMinimal/Startup.cs
+++ AspMinimal/Startup.cs
@@ -9,7 +9,6 @@ namespace AspMinimal
     {
         public void Configuration(IAppBuilder app)
         {
-           
         }
     }
 }
